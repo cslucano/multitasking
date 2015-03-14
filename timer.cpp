3adf8edad05dc030a6221bfeb90797cfbdbde468
Carlos Salvatierrra <cslucano@gmail.com>
2015-03-14 07:50:39 +0000

    added timer module

--- timer.cpp
+++ timer.cpp
@@ -1,12 +1,46 @@
-#include "utimer.h"
+#include "utime.h"
 
+long timediff(struct timespec start struct timespec end) {
+  long acum = 1e3 * (end.tv_sec -start.tv_sec);
+  acum += (end.tv_nsec - start.tv_nsec)/1e3;
 
-void timerBegin(void) {
-	
+  return acum;
+}
+
+void timerBegin(int time_up, int time_down) {
+  clock(CLOCK_REALTIME, &last);	
+  timeState = STATE_DOWN;
+  t_up      = time_up;
+  t_down    = time_down
 }
 
 void timerRun(void) {
-	
+  clock(CLOCK_REALTIME, &now);
+  switch(timerState) {
+    case TIMER_STATE_EMBRY:
+      break;
+    case TIMER_STATE_RUNNING:
+      switch(timeState) {
+        case STATE_UP:
+          if(diff(last, now) > t_up) {
+            timeState = STATTE_DOWN;
+          }
+          break;
+        case STATE_DOWN:
+          if(diff(last, now) > t_down) {
+            timeState = STATE_UP;
+          }
+          break;
+      }
+      last = now;
+      break;
+    case TIMER_STATE_BLOCKED:
+      break;
+    case TIMER_STATE_WAITING:
+      break;
+    case TIMER_STATE_ZOMBY:
+      break;
+  }
 }
 
 void ledOn(void);
--- timer.h
+++ timer.h
@@ -1,13 +1,16 @@
 #include <Arduino.h>
-#define TIMER_ESTADO_EMBRY    0
-#define TIMER_ESTADO_RUNNING  1
-#define TIMER_ESTADO_BLOCKED  2
-#define TIMER_ESTADO_WAITING  3
-#define TIMER_ESTADO_ZOMBY    4
+#define TIMER_STATE_EMBRY    0
+#define TIMER_STATE_RUNNING  1
+#define TIMER_STATE_BLOCKED  2
+#define TIMER_STATE_WAITING  4
+#define TIMER_STATE_ZOMBY    8
+#define STATE_DOWN           0
+#define STATE_UP             1
 
+int t_up;
+int t_down;
 
-
-void timerBegin(void);
+void timerBegin(100, 100);
 void timerRun(void);
 
 void ledOn(void);
